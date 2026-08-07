ec6aba46d33b3113c6878ac3d122ec2494340295
Quiel <ing.jeremy.quiel@gmail.com>
2026-08-07 10:23:02 -0600

    GIT INIT

---
+++ FirstAPI/src/main/java/com/learningSpringBoot/FirstAPI/controller/saludoController.java
@@ -0,0 +1,17 @@
+package com.learningSpringBoot.FirstAPI.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+// import org.springframework.web.bind.annotation.RequestParam;
+
+
+@RestController
+public class saludoController {
+
+    @GetMapping("/saludo")
+    public String saludo() {
+        return "Hola Mundo";
+    }
+    
+
+}
